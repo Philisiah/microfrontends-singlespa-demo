7cabe80c606f2af06c49207e76073453516b225d
Philisiah <sphilisiah@gmail.com>
2019-06-19 14:07:41 +0000

    React App

---
+++ src/react/root.component.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+const MainPage = () =>
+    <section className="section">
+      <div className="container">
+        <div className="tile is-ancestor">
+          <div className="tile is-parent">
+            <article className="tile is-child ">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce fermentum placerat nunc vitae
+              condimentum. Sed fringilla in lectus vel cursus. Etiam sit amet iaculis est. Cras luctus dignissim
+              dapibus. Donec eget pellentesque felis, quis laoreet nulla. Suspendisse quam urna, porttitor vitae
+              tempor quis, dictum at purus. Donec a dui purus. Praesent semper, est ut ultricies tristique, nunc
+              ligula consequat ex, efficitur posuere massa dui ut sapien. Cras fermentum pellentesque tristique. Nam
+              vitae purus
+            </article>
+          </div>
+          <div className="tile is-6 is-parent">
+            <article className="tile is-child">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce fermentum placerat nunc vitae
+              condimentum. Sed fringilla in lectus vel cursus. Etiam sit amet iaculis est. Cras luctus dignissim
+              dapibus. Donec eget pellentesque felis, quis laoreet nulla. Suspendisse quam urna, porttitor vitae
+              tempor quis, dictum at purus. Donec a dui purus. Praesent semper, est ut ultricies tristique, nunc
+              ligula consequat ex, efficitur posuere massa dui ut sapien. Cras fermentum pellentesque tristique. Nam
+              vitae purus augue
+            </article>
+          </div>
+          <div className="tile is-vertical is-parent">
+            <article className="tile is-child ">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce fermentum placerat nunc vitae
+              condimentum. Sed fringilla in lectus vel cursus. Etiam sit amet iaculis est. Cras luctus dignissim
+              dapibus. Donec eget pellentesque felis, quis laoreet nulla. Suspendisse quam urna, porttitor vitae
+              tempor quis, dictum at purus. Donec a dui purus. Praesent semper, est ut ultricies tristique, nunc
+              ligula consequat ex, efficitur posuere massa dui ut sapien. Cras fermentum pellentesque tristique. Nam
+              vitae purus
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+// <div className="w3-container w3-margin-top w3-padding">
+//
+//     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce fermentum placerat nunc vitae
+//             condimentum. Sed fringilla in lectus vel cursus. Etiam sit amet iaculis est. Cras luctus dignissim
+//             dapibus. Donec eget pellentesque felis, quis laoreet nulla. Suspendisse quam urna, porttitor vitae
+//             tempor quis, dictum at purus. Donec a dui purus. Praesent semper, est ut ultricies tristique, nunc
+//             ligula consequat ex, efficitur posuere massa dui ut sapien. Cras fermentum pellentesque tristique. Nam
+//             vitae purus augue. Mauris tristique nisi diam, non suscipit magna vehicula eget. Integer mauris ipsum,
+//             luctus dapibus nulla ut, sagittis placerat enim. Phasellus nulla tortor, ullamcorper ac nulla eu,
+//             laoreet tincidunt turpis. Integer nec fringilla lectus, nec commodo ligula. Nullam a ante sed augue
+//             sagittis consequat in vel augue.</p>
+// </div>
+
+export default MainPage;
